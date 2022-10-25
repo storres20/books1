57ddbbf76948250df49c6e665138691b934b2764
PedroH-Neves <PedroHDN710@gmail.com>
2022-10-25 12:13:55 -0400

    Add remove function

--- index.js
+++ index.js
@@ -50,3 +50,21 @@ addButton.addEventListener('click', (e) => {
     localStorage.setItem('books', JSON.stringify(books));
   }
 });
+
+/* remove button */
+const remove = document.querySelectorAll('.remove');
+
+remove.forEach((item) => {
+  item.addEventListener('click', () => {
+    item.parentElement.remove();
+    const bookname = item.name;
+
+    /* remove from localStorage */
+    const getremove = JSON.parse(localStorage.getItem('books'));
+
+    const newArr = getremove.filter((object) => object.title !== bookname);
+
+    /* update localstorage */
+    localStorage.setItem('books', JSON.stringify(newArr));
+  });
+});
