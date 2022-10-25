adab7311fc6036dcbfd24dd08b4c6af9c7081b9b
PedroH-Neves <PedroHDN710@gmail.com>
2022-10-25 14:26:18 -0400

    Correct linters errors

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-classes-per-file */
+
 const addButton = document.getElementById('addButton');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
@@ -7,7 +9,6 @@ const addBook = document.getElementById('addBook');
 /* addBook - hold books after every refresh page */
 if (localStorage.getItem('books') !== null) {
   const getbook = JSON.parse(localStorage.getItem('books'));
-  console.log(getbook);
 
   getbook.forEach((item) => {
     addBook.innerHTML += `
@@ -27,16 +28,15 @@ class Book {
   }
 }
 
-class methods {
-  addbook(book1) {
+class Methods {
+  static addbook(book1) {
     addBook.innerHTML += `
     <div class="books">
       <p>"${title.value}" by "${author.value}"</p>
       <button class="remove" name="${title.value}">Remove</button>
     </div>
   `;
-    console.log('hello')
-    
+
     if (localStorage.getItem('books') === null) {
       const books = [];
       books.push(book1);
@@ -48,20 +48,20 @@ class methods {
     }
   }
 
-  removebook() {
+  static removebook() {
     /* remove button */
     const remove = document.querySelectorAll('.remove');
-    
+
     remove.forEach((item) => {
       item.addEventListener('click', () => {
         item.parentElement.remove();
         const bookname = item.name;
-    
+
         /* remove from localStorage */
         const getremove = JSON.parse(localStorage.getItem('books'));
-    
+
         const newArr = getremove.filter((object) => object.title !== bookname);
-    
+
         /* update localstorage */
         localStorage.setItem('books', JSON.stringify(newArr));
       });
@@ -70,13 +70,13 @@ class methods {
 }
 
 /* initialize method class */
-const ui = new methods();
+const ui = new Methods();
 
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
 
   const book1 = new Book(title.value, author.value);
-  
+
   /* add book with method class */
   ui.addbook(book1);
 });
